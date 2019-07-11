c13777cde36fcef1509fb57062fda974919c6053
Yaroslav <doctor355@yahoo.com>
2019-07-11 12:47:15 +0300

    change name

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>ЭТО ЁБАНЫЙ ПИЗДЕЦ А НЕ ПРОЕКТ</div>;
+  return <div><h1>Это жикая ёбань, которая весит пол ГБ</h1></div>;
 }
 
 export default App;
